4c4ff452d85233200fd46f840a34f77bb34281dc
Cedricium <amaya.cedric@gmail.com>
2019-08-26 17:02:43 -0700

    Define Users model and router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+const userRouter = require('./users/users-router')
+
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/', userRouter)
 
 app.get('/', (req, res) => {
   res.json({
