6174f0dbac15873402400023714fd02b059ce0ef
Cedricium <amaya.cedric@gmail.com>
2019-08-26 17:13:33 -0700

    Implement persistence using sessions

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session)
 
 const userRouter = require('./users/users-router')
 
+const app = express()
+
+const sessionOptions = {
+  name: 'thismycookie',
+  secret: 'ireallyreallylikecookies',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new knexSessionStore({
+    knex: require('./database/dbConfig.js'),
+    tablename: 'session',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}
+
 app.use(cors())
 app.use(express.json())
+app.use(session(sessionOptions))
 
-app.use('/api/', userRouter)
+app.use('/api', userRouter)
 
 app.get('/', (req, res) => {
   res.json({
